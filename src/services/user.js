2928cae2e5b4a27f05de86c1ac49a4a0a5235319
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-05-23 19:14:53 -0300

    user service added

---
+++ src/services/user.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const User = require('../database/models/user');
+const ClientError = require('../errors/clientError');
+const ServerError = require('../errors/serverError');
+const env = require('../config/env');
+
+class UserService {
+  static createUser({ name, email, password }) {
+    const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    if (!validEmail) throw new ClientError('Email inválido');
+    if (!name) throw new ClientError('Nome não existe');
+    if (!password) throw new ClientError('Senha inválida');
+    const passwordHash = bcrypt.hashSync(password, env.saltRounds);
+    return User.create({
+      name,
+      email,
+      passwordHash,
+    }).catch(() => {
+      throw new ServerError('Erro ao criar conta');
+    });
+  }
+}
+
+module.exports = UserService;
