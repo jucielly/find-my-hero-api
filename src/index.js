937e243b54d552519906eabd4685b8048247c089
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-05-23 16:54:59 -0300

    express config added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const env = require('./config/env');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (request, response, next) => {
+  response.json({ teste: 'hellou' });
+});
+
+app.listen(env.port, () => {
+  console.log('server started!', env.port);
+});
